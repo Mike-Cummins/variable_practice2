a547871931287043fbe6b7a386ea3890f3b68ff3
Mike Cummins <Cummins.Michael.R@gmail.com>
2022-08-25 14:25:54 -0700

    Reassigns string variables, adds puts

--- variables.rb
+++ variables.rb
@@ -1,15 +1,34 @@
 name = "Mike"
+name # "Mike"
+name = "Cozmo"
+puts name
 city = "Bend"
+city # "Bend"
+city = "Vancouver"
+puts city
 state = "Oregon"
+state # "Oregon"
+state = "Washington"
+puts state
 
 age = 33
 weight_in_pounds = 200
 number_of_pets = 2
+puts age
+puts weight_in_pounds
+puts number_of_pets
+
 
 height_in_feet = 6.25
 average_pet_age = 10.5
 motorcycles_owned = 2.0
+puts height_in_feet
+puts average_pet_age
+puts motorcycles_owned
 
 pale_skin = true
 has_tattoos = true
 has_piercings = true
+puts pale_skin
+puts has_tattoos
+puts has_piercings
